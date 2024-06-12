817c814594d723badc5404c94bf2ddd1bd038a62
Justin Beri <18635834+justinberi@users.noreply.github.com>
2024-06-13 09:39:28 +1000

    Updates

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,13 +8,18 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut r = self.clone();
+        r.push(String::from("Bar"));
+        r
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -22,7 +27,8 @@ mod tests {
 
     #[test]
     fn is_vec_pop_eq_bar() {
-        let mut foo = vec![String::from("Foo")].append_bar();
+        let mut bar  = vec![String::from("Foo")];
+        let mut foo = bar.append_bar();
         assert_eq!(foo.pop().unwrap(), String::from("Bar"));
         assert_eq!(foo.pop().unwrap(), String::from("Foo"));
     }
